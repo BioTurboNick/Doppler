4f3d2909585a19d24a07b9f9ae840cf4f16cfced
Nick Bauer <nicholasbauer@outlook.com>
2018-01-23 14:03:18 -0500

    Added color channel label

---
+++ Doppler/ValueConverters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Doppler.ValueConverters
+{
+    public class ColorToSolidColorBrushConverter :
+        IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (targetType != typeof(Brush) || !(value is Color colorValue))
+                return DependencyProperty.UnsetValue;
+
+            return new SolidColorBrush(colorValue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
